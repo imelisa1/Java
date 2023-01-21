cdbaff508735866890f89b9eb812778aa18c2946
İrem Melisa Taş <97853397+imelisa1@users.noreply.github.com>
2023-01-21 13:36:10 +0300

    Add files via upload
    first step and basics about java

---
+++ java1/class1.java
@@ -0,0 +1,8 @@
+public class class1 {
+    // bu class ismi projenin ismiyle aynı olmak zorunda.
+    public static void main(String[] args){
+        // main methodu olmak zorunda.
+        //System.out.println("hello java");
+        // ekrana bastır ve alt satıra geç.
+    }
+}
