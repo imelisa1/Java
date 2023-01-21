3a8a65d17795666b820213a60ac774c6191404bb
İrem Melisa Taş <97853397+imelisa1@users.noreply.github.com>
2023-01-21 13:33:58 +0300

    Add files via upload

---
+++ JavaApplication1/src/string.java
@@ -0,0 +1,15 @@
+
+public class string {
+    public static void main(String[] args){
+        
+        String m= "hello";
+        //System.out.println(m+ "java");
+        
+        String a= "java "
+                + "programlama "
+                + "giriş";
+        
+        System.out.println(a);
+    }
+    
+}
