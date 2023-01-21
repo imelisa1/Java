8de57e74b47e9cd432b7c61b70778fc41a3c1c21
İrem Melisa Taş <97853397+imelisa1@users.noreply.github.com>
2023-01-21 13:34:36 +0300

    Delete JavaApplication1 directory

--- JavaApplication1/src/class1.java
+++
@@ -1,8 +0,0 @@
-public class class1 {
-    // bu class ismi projenin ismiyle aynı olmak zorunda.
-    public static void main(String[] args){
-        // main methodu olmak zorunda.
-        //System.out.println("hello java");
-        // ekrana bastır ve alt satıra geç.
-    }
-}
