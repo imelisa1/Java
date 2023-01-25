1e376272d56242cba0c0f81257279fae480c441e
İrem Melisa Taş <97853397+imelisa1@users.noreply.github.com>
2023-01-25 19:52:36 +0300

    Add files via upload

---
+++ OPPwithJava/test_of_account.java
@@ -0,0 +1,15 @@
+
+public class test_of_account {
+    public static void main(String[] args) {
+        
+        account account1 = new account();
+        
+        account account2 = new account("123213",1000.0,"Mustafa Murat CoÅŸkun","coskun.m.murat@gmail.com","324324");
+        
+       // account2.paraYatir(500);
+        
+       System.out.print(account1.getEmail());
+        
+    }
+    
+}
